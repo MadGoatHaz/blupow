6cd7f48215c7a9c2920ec113d004d10bb97738ca
MadGoatHaz <ghazlett@gmail.com>
2025-06-19 09:53:55 -0600

    fix: Correct config flow for HA managed connections

--- config_flow.py
+++ config_flow.py
@@ -6,6 +6,7 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant.components.bluetooth import (
+    BluetoothServiceInfo,
     async_discovered_service_info,
 )
 from homeassistant.config_entries import ConfigFlow
@@ -24,37 +25,39 @@ class BluPowConfigFlow(ConfigFlow, domain=DOMAIN):
 
     def __init__(self) -> None:
         """Initialize the config flow."""
+        self._discovery_info: BluetoothServiceInfo | None = None
         self._discovered_devices: dict[str, str] = {}
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
-        """Handle the user step to discover and select a device."""
+        """Handle the user step to select a device."""
         if user_input is not None:
             address = user_input[CONF_ADDRESS]
-            await self.async_set_unique_id(address)
+            await self.async_set_unique_id(address, raise_on_progress=False)
             self._abort_if_unique_id_configured()
             
-            title = self._discovered_devices.get(address, address)
-            return self.async_create_entry(title=title, data={CONF_ADDRESS: address})
+            name = self._discovered_devices.get(address, "Unknown Device")
+            
+            return self.async_create_entry(title=name, data={CONF_ADDRESS: address})
 
         # Discover devices
         current_discoveries = async_discovered_service_info(self.hass, connectable=True)
-        for discovery_info in current_discoveries:
-            address = discovery_info.address
-            name = discovery_info.name
-            if name and (
-                name.upper().startswith("REGO")
-                or name.upper().startswith("RENOGY")
-                or name.upper().startswith("BT-")
+        for discovery in current_discoveries:
+            if (
+                discovery.name.upper().startswith("REGO")
+                or discovery.name.upper().startswith("RENOGY")
+                or discovery.name.upper().startswith("BT-")
             ):
-                self._discovered_devices[address] = name
+                self._discovered_devices[discovery.address] = discovery.name
 
         if not self._discovered_devices:
             return self.async_abort(reason="no_devices_found")
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required(CONF_ADDRESS): vol.In(self._discovered_devices)}),
+            data_schema=vol.Schema(
+                {vol.Required(CONF_ADDRESS): vol.In(self._discovered_devices)}
+            ),
         )
 
